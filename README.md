24a93f7abec6eaa836f4342c146b93bef41a7dda
Krasser Tamás <krasser.tamas@gmail.com>
2015-07-14 18:56:26 +0200

    ADD JSDoc comments

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Forked from udacity frogger project.
 
 Multiplayer version, you can play against your friend.
 
+To play just click on index.html or visit
+http://csiszi.github.io/frontend-nanodegree-arcade-game
+
 Start the game with selecting the number of players (1 or 2 with keys 1 or 2).
 
 Controls:
-
 - Player 1: Arrows
 - Player 2: W S A D
--- js/resources.js
+++ js/resources.js
@@ -1,3 +1,4 @@
+'use strict';
 /* Resources.js
  * This is simple an image loading utility. It eases the process of loading
  * image files so that they can be used within your game. It also includes
@@ -6,7 +7,7 @@
  */
 (function() {
     var resourceCache = {};
-    var loading = [];
+    // var loading = [];
     var readyCallbacks = [];
 
     /* This is the publicly accessible image loading function. It accepts
